1fba537014accfdc043f58370d85bf7ae9b77efd
DiogoDSJ <107954198+DiogoDSJ@users.noreply.github.com>
2024-07-10 09:49:01 -0300

    feat: Implementando colisão e thread do botão

--- Main.c
+++ Main.c
@@ -9,15 +9,22 @@
     #include <inttypes.h>
     #include <linux/input.h>
     #include <pthread.h>
+    #include <semaphore.h>
+
+    #include <sys/mman.h>
+    #include "address_map_arm.h"
                 
+
+            
     pthread_t threads[10];
+   // sem_t semaforo; // Declaração do semáforo
 
 
     #define MOUSE_DEV "/dev/input/event0"
 
     int fd;
     void *mousefik(void *arg){
-
+        //sem_wait(&semaforo);
         uint32_t dataA = 0;
         uint32_t dataB = 0;
 
@@ -46,17 +53,175 @@
                 coodx += ev.value;
             }
 
-            
+            //colisao do monitor
+            if(coodx < 0) coodx = 0;
+            if(coodx > 619) coodx= 619;
+            if(coody > 459) coody = 459;
+            if(coody < 0) coody = 0;
+
+            //colisao muro esquerdo
+            if(coodx <= 37){
+                coodx -= ev.value;
+            }
+            //colisao muro direito
+        
+            if((coody >= 210 && coody <= 230) && (coodx>= 102 && coodx<= 200)){
+                if(ev.code == 0 ) coodx -= ev.value;
+                else coody -= ev.value;
+            }
+            if((coody >= 231 && coody <= 342) && (coodx >= 181 && coodx <= 463)){
+                if(ev.code == 0 ) coodx -= ev.value;
+                else coody -= ev.value;
+            }
+            else if(((coody < 101) && coodx >= 165) || ((coody >= 101 && coody <= 230) && (coodx >= 101 && coodx <= 180))){
+                if(ev.code == 0 ) coodx -= ev.value;
+                else coody -= ev.value;
+            }   // 544 e y 230
+                // 478 e y 388
+            if((coodx >= 613) && (coody > 132 && coody <= 389)){
+                if(ev.code == 0) coodx -= ev.value;
+                else coody -= ev.value;
+            }
+            if((coodx > 470 && coodx < 559) && (coody > 205 && coody < 230)){
+                if(ev.code == 0) coodx -= ev.value;
+                else coody -= ev.value;
+            }
+            if((coodx > 550 && coodx < 630) && (coody < 134 && coody > 100 )){
+                if(ev.code == 0) coodx -= ev.value;
+                else coody -= ev.value;
+            }
+            if((coodx > 470 && coodx <= 477) && (coody > 230 && coody < 340)){
+                if(ev.code == 0) coodx -= ev.value;
+                else coody -= ev.value;
+            }
+            // colisao teto
+            if(coody <= 6){
+                coody -= ev.value;
+            }
+            // colisao chao de la de baixo
+            if(coody >= 389){
+                coody -= ev.value;
+            }
+            if((coody <= 342 || coody >= 389) && (coodx > 181 && coodx < 480)) { // 343 e 389{
+                coody -= ev.value;
+            }
+
+
+            //ultima parte
+
+            printf("x: %d, y: %d\n", coodx, coody);
+            print_sprite(fd, &dataA, &dataB, 1, coodx, coody, 25, 1);
+            // Processa o evento recebido
+            if (ev.type == EV_REL) {
+                // Eventos de movimento relativo do mouse
+                if (ev.code == REL_X || ev.code == REL_Y) {
+                    printf("Movimento relativo: eixo %d, valor %d\n", ev.code, ev.value);
+                } else if (ev.code == REL_WHEEL) {
+                    printf("Roda do mouse: valor %d\n", ev.value); //irrelevante
+                }
+            } else if (ev.type == EV_KEY) {
+                // Eventos de botão do mouse
+                if (ev.code == BTN_LEFT || ev.code == BTN_RIGHT || ev.code == BTN_MIDDLE) {
+                    if (ev.value == 1)
+                        printf("Botão %d pressionado\n", ev.code);
+                }
+            } 
+        }
 
         close(mouse);
+        //sem_post(&semaforo); // Libera o semáforo
+        //pthread_exit(NULL);
         
     }
 
+
+
+
+
+
+
+
+    void *botao(void *arg){
+        //sem_wait(&semaforo);
+
+        volatile int *KEY_ptr; // virtual address pointer to red LEDs
+        int fd = -1; // used to open /dev/mem
+        void *LW_virtual; // physical addresses for light-weight bridge
+
+        // Open /dev/mem to give access to physical addresses
+        if ((fd = open("/dev/mem", (O_RDWR | O_SYNC))) == -1) {
+            printf("ERROR: could not open \"/dev/mem\"...\n");
+            return (-1);
+        }
+
+        // Get a mapping from physical addresses to virtual addresses
+        LW_virtual = mmap(NULL, LW_BRIDGE_SPAN, (PROT_READ | PROT_WRITE), MAP_SHARED, fd, LW_BRIDGE_BASE);
+        if (LW_virtual == MAP_FAILED) {
+            printf("ERROR: mmap() failed...\n");
+            close(fd);
+            return (-1);
+        }
+
+        // Set virtual address pointer to I/O port
+        KEY_ptr = (int *)(LW_virtual + 0x0);
+
+        int estado = 1;
+            
+        while(1){
+
+            if (*KEY_ptr == 15 && estado == 0){
+                printf("clicou\n");
+                estado = 1;
+            } 
+            if (estado == 1 && *KEY_ptr != 15){
+                estado = 0;
+            }
+            
+        }
+        
+        
+
+        // Close the previously-opened virtual address mapping
+        if (munmap(LW_virtual, LW_BRIDGE_SPAN) != 0) {
+            printf("ERROR: munmap() failed...\n");
+            return (-1);
+        }
+
+        // Close /dev/mem to give access to physical addresses
+        close(fd);
+        //sem_post(&semaforo); // Libera o semáforo
+        //pthread_exit(NULL);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     int main_mouse(){
+        //sem_init(&semaforo, 0, 1);
         pthread_create(&(threads[0]),NULL,mousefik,NULL);
+        pthread_create(&(threads[1]),NULL,botao,NULL);
 
 
         pthread_join(threads[0],NULL);
+        pthread_join(threads[1],NULL);
+
+        
+        // Destroi o semáforo
+        //sem_destroy(&semaforo);
         
         return 0;
 
@@ -71,6 +236,9 @@
             uint32_t dataB = 0;
 
             
+
+            print_sprite(fd, &dataA, &dataB, 1, 100, 240, 2, 1);
+
             print_sprite(fd, &dataA, &dataB, 1, 320, 240, 25, 1);
             set_background(fd, &dataA,&dataB, 0, 5, 6);
             /*
@@ -90,7 +258,19 @@
             for(int i = 5; i<= 22; i++){
                 editar_bloco_background(fd, &dataA,&dataB, i, 14, 7, 7, 7);
             }
-  
+        /*
+            for(int i = 5; i <= 12; i++){
+                editar_bloco_background(fd, &dataA,&dataB, i, 16, 7, 7, 7);
+            }
+
+            for(int i = 16; i <= 49; i++){
+                editar_bloco_background(fd, &dataA,&dataB, 13, i, 7, 7, 7);
+            }
+
+            for(int i = 5; i <= 12; i++){
+                editar_bloco_background(fd, &dataA,&dataB, i, 47, 7, 7, 7);
+            }
+        */
 
             //parte da entrada
             for(int j = 1; j <= 13; j++){
@@ -249,7 +429,81 @@
                 }   
             }
         
-    
+    /*
+        int mouse;
+        struct input_event ev;
+
+        printf("Iniciando leitura de eventos do mouse...\n");
+
+        // Abre o dispositivo de entrada (por exemplo, um mouse)
+        mouse = open(MOUSE_DEV, O_RDONLY);
+        if (mouse == -1) {
+            perror("Erro ao abrir o dispositivo de entrada");
+            exit(EXIT_FAILURE);
+        }
+        int coodx = 50, coody = 50;
+        while (1) {
+            // Lê eventos do dispositivo de entrada
+            if (read(mouse, &ev, sizeof(struct input_event)) == -1) {
+                perror("Erro ao ler evento");
+                exit(EXIT_FAILURE);
+            }
+            if(ev.code == 1){
+                coody += ev.value;
+            }
+            else if(ev.code == 0){  
+                coodx += ev.value;
+            }
+
+            //colisao do monitor
+            if(coodx < 0) coodx = 0;
+            if(coodx > 619) coodx= 619;
+            if(coody > 459) coody = 459;
+            if(coody < 0) coody = 0;
+
+            //colisao muro esquerdo
+            if(coodx <= 37){
+                coodx -= ev.value;
+            }
+            //colisao muro direito
+        
+            
+            if((coody <= 6 || coody > 100) && coodx >= 164){
+                coodx -= ev.value;
+                if(ev.code == 0) coody -= ev.value;
+            } 
+            // colisao teto
+            if(coody <= 6){
+                coody -= ev.value;
+            }
+            // colisao chao de la de baixo
+            if(coody >= 389){
+                coody -= ev.value;
+            }
+            if((coody <= 343 || coody >= 389) && (coodx > 179 && coodx < 480)) { // 343 e 389{
+                coody -= ev.value;
+            }
+            printf("x: %d, y: %d\n", coodx, coody);
+            print_sprite(fd, &dataA, &dataB, 1, coodx, coody, 25, 1);
+            // Processa o evento recebido
+            if (ev.type == EV_REL) {
+                // Eventos de movimento relativo do mouse
+                if (ev.code == REL_X || ev.code == REL_Y) {
+                    printf("Movimento relativo: eixo %d, valor %d\n", ev.code, ev.value);
+                } else if (ev.code == REL_WHEEL) {
+                    printf("Roda do mouse: valor %d\n", ev.value); //irrelevante
+                }
+            } else if (ev.type == EV_KEY) {
+                // Eventos de botão do mouse
+                if (ev.code == BTN_LEFT || ev.code == BTN_RIGHT || ev.code == BTN_MIDDLE) {
+                    if (ev.value == 1)
+                        printf("Botão %d pressionado\n", ev.code);
+                }
+            } 
+        }
+
+        close(mouse);
+        */  
         main_mouse();
 
             // Buffer para armazenar a leitura do dispositivo
